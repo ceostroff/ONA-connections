811a616e27ed4a954c60f8e7d80938e7793c66b2
charliesmart <charles.smart@uconn.edu>
2016-09-16 16:51:50 -0600

    Fix final_connections.py

--- data-collection/final-connections.py
+++ data-collection/final-connections.py
@@ -2,24 +2,28 @@ from config import *
 import json
 from pprint import pprint
 
-print('running')
-
 # Open friends JSON file
 with open('data/friends.json') as json_file:
     data = json.load(json_file)
-    
+
 # Create dict to hold final connections
 connections = {}
 
 # Iterate through users in original connections
 for user, friends in data.items():
-    
     # Create an entry for that user in the final conncetions dict
     connections[user] = []
 
     # For each person that user follows...
     for friend in friends:
-        connections[user].append(str(friend))
+        
+        # Check if a conncetion between them is already logged.
+        # If not, create that connection.
+        try:
+            if int(user) in data[str(friend)]:
+                connections[user].append(str(friend))
+        except KeyError:
+            pass
 
 # Print everything to a JSON
 with open('data/final_connections.json', 'w') as json_out:
