af65a623abb8c72ae7cb85e0d646c76e510ec481
aaronja38 <Aaron@host41.dnvrcc2.hyatthsiaq.com>
2016-09-16 13:11:30 -0600

    Added Submit app view

--- app/app.py
+++ app/app.py
@@ -4,13 +4,17 @@ from flask_script import Manager
 app = Flask(__name__)
 manager = Manager(app)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
 @app.route('/about/')
 def about():
     return render_template('about.html')
+
+@app.route("/submit", methods=["POST"])
+def submit_handler():
+    return {user_network}
 
 
 
