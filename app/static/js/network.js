9ab8a756e64f81bc37eebba144899906af7cc63d
charliesmart <charles.smart@uconn.edu>
2016-09-16 13:41:05 -0600

    Add AJAX form submission in JS

--- app/static/js/network.js
+++ app/static/js/network.js
@@ -1,3 +1,53 @@
 $(document).ready(function() {
+
+    var data;
     
+    d3.csv('static/js/twitter_details.csv', function(csv) {
+	data = csv;
+	searchInit();
+    });
+
+    console.log(data);
+
+    function searchInit() {
+	$('.twitter-submit').submit(function(e) {
+	    e.preventDefault();
+	    
+	    var twitterName = $(this).find('#username').val(),
+	        result;
+
+	    console.log(twitterName + '\n');
+	    
+	    if (twitterName[0] == '@') {
+		result = $.grep(data, function(e, i){
+		    if (e.username.toLowerCase() == twitterName.toLowerCase()) {
+			return data[i]['id'];
+		    }
+		});
+	    } else {
+		result = $.grep(data, function(e, i){
+		    if (e.name.toLowerCase() == twitterName.toLowerCase()) {
+			return data[i]['id'];
+		    }
+		});
+	    }
+
+	    sendAjax(result);
+	});
+    }
 });
+
+function sendAjax(id) {
+    $.ajax({
+	type: 'POST',
+	url: '/submit',
+	data: {"id": id},
+	datatype: "JSON",
+	success: function(result) {
+	    console.log(result);
+	},
+	error: function(request, status, error) {
+	    console.log(status);
+	}
+    })
+}
