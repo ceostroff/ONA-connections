3f7a85950e7405ecb9ac739b27badec44ade41b3
charliesmart <charles.smart@uconn.edu>
2016-09-16 09:35:51 -0600

    Finish running friends collection script

--- data-collection/friends.py
+++ data-collection/friends.py
@@ -1,7 +1,7 @@
 from config import *
 import twitter
 import unicodecsv as csv
-import datetime
+from datetime import datetime
 import time
 import json
 
@@ -25,7 +25,8 @@ def def_api(i):
         consumer_key = consumer_keys[i],
         consumer_secret = consumer_secrets[i],
         access_token_key = access_tokens[i],
-        access_token_secret = access_token_secrets[i]
+        access_token_secret = access_token_secrets[i],
+        sleep_on_rate_limit=True
     )
 
 api = def_api(api_key_set)
@@ -33,36 +34,38 @@ api = def_api(api_key_set)
 connections = {}
 
 num_in_key = 0
+            
+for index, user in enumerate(users[700:1500]):
 
-for index, user in enumerate(users):
-    connections[user] = []
-    user_friends = api.GetFriendIDs(user_id=user)
-
-    print(str(index) + '\n')
-    print(user_friends)
-    print('\n')
     
-    for friend in user_friends:
-        if friend in users:
-            connections[user].append(friend)
-
+    
+    connections[user] = []
+    try:
+        user_friends = api.GetFriendIDs(user_id=user)
+        
+        print('Getting friends of user #' + str(index))
+        
+        for friend in user_friends:
+            if str(friend) in users:
+                connections[user].append(friend)
+                
+    except twitter.error.TwitterError, e:
+        print(e)
+    except IOError:
+        pass
+    
     num_in_key += 1
 
-    if (num_in_key > 14):
+    with open('data/friends_2.json', 'w') as json_out:
+        json.dump(connections, json_out)
+
+    if (num_in_key > 13):
         num_in_key = 0
         
         if (api_key_set < keys_length):
             api_key_set += 1
         else:
-            end_time = datetime.datetime.now()
-            time_dif = (start_time - end_time).total_seconds()
-
-            if (time_dif < 900):
-                time.sleep(900 - time_dif + 20)
-                
             api_key_set = 0
 
         api = def_api(api_key_set)
-    
-with open('data/friends.json', 'w') as json_out:
-    json.dump(connections, json_out)
+
