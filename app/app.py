e9ec7e39dca51c011c7eff39586b659c3aa9402e
aaronja38 <Aaron@host41.dnvrcc2.hyatthsiaq.com>
2016-09-16 12:55:54 -0600

    App Manager and Template Changes

--- app/app.py
+++ app/app.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template
+from flask_script import Manager
 
 app = Flask(__name__)
+manager = Manager(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
 
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
